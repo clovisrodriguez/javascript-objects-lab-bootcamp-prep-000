a3f567db73e0c14799469a9f906585eeb700c6be
Clovis Rodríguez <clovis1992@gmail.com>
2018-01-22 23:48:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const recipes = {
   
 };
 
-function updateObjectWithKeyAndValue (object, key, value) {
+```function updateObjectWithKeyAndValue (object, key, value) {
   object[key] = value
   return object
 }
