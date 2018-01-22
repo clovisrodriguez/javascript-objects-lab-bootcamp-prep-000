89793aa29cbc93db2a1f6aa53777ed652808c07d
Clovis Rodríguez <clovis1992@gmail.com>
2018-01-22 23:38:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const recipes = {
 function updateObjectWithKeyAndValue (object, key, value) {
   object[key] = value
   return object
-  Object.assign
+  Object.assign({})
 }
 
 updateObjectWithKeyAndValue
